e2b3e7f31856843b76137368c58b305e374cfe7f
Emrah Şafak <celalemrah5@gmail.com>
2025-09-24 16:10:46 +0300

    Update variable_rules.cpp

--- lesson-02-variables/variable_rules.cpp
+++ lesson-02-variables/variable_rules.cpp
@@ -1,1 +1,23 @@
-variable_rules.cpp
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	
+
+	// Degisken isimlerinde bagzi kurallar://
+	
+	// - Degisken isimlerinde ilk karakter kesinlikle rakam olamaz.
+	// - Degisken isimlerinde Turkce karakterler kullanılmaz.
+	// - Degisken isimlerindearada bosluk olamaz.
+	// - Degiskenisimlerinde arada nokta vb. karakterler kullanilamaz.
+
+	// Degisken isimlerini camelCase standartlarina uygun yazmaniz tavsiye olunur.
+
+
+	int dogumYili = 1980;
+	int birinciSinavNotu = 97;
+				
+	return 0;
+}
