a1ee8194bbb1c6f309b816f8dc2463ec81dff4e1
Emrah Şafak <celalemrah5@gmail.com>
2025-09-26 23:49:32 +0300

    Update and rename int_string_float_double.cpp to int_string_float_double_bool_char.cpp

--- lesson-03-dataTypes/int_string_float_double_bool_char.cpp
+++ lesson-03-dataTypes/int_string_float_double_bool_char.cpp
@@ -8,11 +8,21 @@ int main()
 	string isim = "Kaan Kaya";
 	float sayisalNotOrtalamasi = 3.47f;
 	double pi = 3.14159265359;
+	bool sinifiGectiMi = false;
+	//char notOrtalamasiHarf = 'B'; // char da tek tırnak kullanılır, " ASCII " tablosundan faydalanarak rakamda verilebilir - "char notOrtalamasiHarf = 65;"  örnek 65-A 66-B 67-C 68-D tekabül eder.
+	 char notOrtalamasiHarf = 68;
+	
+
+
 
 	cout << "Merhaba sayin" << isim << endl;
 	cout << "Dogum Yili: " << dogumYili << endl;
 	cout << "Sayisal Not Ortalamasi: " << sayisalNotOrtalamasi << endl;
 	cout << "Pi nin sayisal degeri: " << pi << endl;
+	cout << "Sinifi Gectiniz mi? " << sinifiGectiMi << endl;
+	cout << "Not Ortalamasi " << notOrtalamasiHarf << endl;
+
+
 
 
      return 0;
