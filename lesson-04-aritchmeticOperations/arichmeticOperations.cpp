969939018bdf1d4ef7d84478b0448cdb555d1c88
Emrah Şafak <celalemrah5@gmail.com>
2025-09-27 01:30:30 +0300

    Update and rename lesson-04-arithmeticOperations to lesson-04-aritchmeticOperations/arichmeticOperations.cpp

---
+++ lesson-04-aritchmeticOperations/arichmeticOperations.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+
+	 int quizNote1 = 72;
+	 float quizNote2 = 51;
+	 //float quizNotOrtalamasi =  (float)(quizNote1 + quizNote2) / 2;
+	 float quizNoteOrtalama = (quizNote1 + quizNote2) / 2;
+
+
+
+	
+	//cout << "Not Ortalamasi " << notOrtalamasiHarf << endl;
+	cout << "Quizler Ortalamasi \n" << quizNoteOrtalama << endl;
+
+
+
+	int x = 12;
+	int y = 10;
+	int z = 15 % 2; // % : mod ( modulus ) operatoru
+
+	cout << (x + z)* y << endl;
+
+
+
+     return 0;
+ 
+}
