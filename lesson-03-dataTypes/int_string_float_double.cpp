b0178bcc16f7f2fcb0f8101d7edeb37e67385896
Emrah Şafak <celalemrah5@gmail.com>
2025-09-26 23:15:52 +0300

    Create int_string_float_double.cpp

---
+++ lesson-03-dataTypes/int_string_float_double.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int dogumYili = 1980; 
+	string isim = "Kaan Kaya";
+	float sayisalNotOrtalamasi = 3.47f;
+	double pi = 3.14159265359;
+
+	cout << "Merhaba sayin" << isim << endl;
+	cout << "Dogum Yili: " << dogumYili << endl;
+	cout << "Sayisal Not Ortalamasi: " << sayisalNotOrtalamasi << endl;
+	cout << "Pi nin sayisal degeri: " << pi << endl;
+
+
+     return 0;
+ 
+}
