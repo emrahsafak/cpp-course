350dcc389052181e0b9f1efb7ede03f1f73ee2e5
Emrah Şafak <celalemrah5@gmail.com>
2025-09-27 18:53:21 +0300

    Create quiz2.cpp

---
+++ lesson-04-numbersStuff/quiz2.cpp
@@ -0,0 +1,14 @@
+include <iostream>
+using namespace std;
+
+int main() 
+{ 
+    int x = 11;
+    int y = x--;
+    int z = 5 + --y;
+    cout << "x: " << x << " y: " << y << " z: " << z << endl;
+    
+    return 0;
+}
+
+// x=10 , y=10 , z=15
