3d0bc05e06c57b76303af6807edd0b17d0716df4
Emrah Şafak <celalemrah5@gmail.com>
2025-09-27 18:50:28 +0300

    Create quiz1.cpp

---
+++ lesson-04-numbersStuff/quiz1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{    
+    int x = 11;
+    int y = x--;
+    int z = 2 * y--;
+    cout << "z: " << z << endl;    
+    
+    return 0;
+}
+
+// Answer = 22 
