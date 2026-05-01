98d3a8d6541c24c31473e018386e6f49ac757d8f
Emrah Şafak <celalemrah5@gmail.com>
2026-05-01 08:03:34 +0300

    Update 2_ExampleWhileLoops.cpp

--- lesson-07-loops/2_ExampleWhileLoops.cpp
+++ lesson-07-loops/2_ExampleWhileLoops.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/* 
+	  Soru: Kullanıcıdan başlangıç ve bitiş sayısını alarak, bu sayılar arasında bulunan ve 7'ye tam bölünebilen sayıları ekrana yazdıran bir program yazınız.  	
+	*/ 
+	
+	int baslangicSayi, bitisSayi;
+	cout << " Baslangic sayinizi giriniz :";
+	cin >> baslangicSayi;
+	cout << " Bitis sayinizi giriniz : ";
+	cin >> bitisSayi;
+
+	while (baslangicSayi <= bitisSayi)
+	{
+		cout << baslangicSayi << endl;
+
+		if (baslangicSayi % 7 == 0)
+		{
+			cout << baslangicSayi << "sayisi 7'ye tam bolunur." << endl;
+		}
+
+		baslangicSayi++;
+
+	}
+    
+}
 
